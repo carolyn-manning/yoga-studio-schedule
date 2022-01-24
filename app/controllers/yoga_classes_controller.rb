df46884a747bd0164aca55f4d642245c72d88fdc
carolyn-manning <carolyn.manning3@gmail.com>
2022-01-24 00:49:54 -0500

    write create action in yoga class controller

--- app/controllers/yoga_classes_controller.rb
+++ app/controllers/yoga_classes_controller.rb
@@ -2,4 +2,25 @@ class YogaClassesController < ApplicationController
     def new 
         @yoga_class = YogaClass.new
     end 
+
+    def create 
+        @yoga_class = YogaClass.new(yoga_class_params)
+        if @yoga_class.save
+            redirect_to yoga_classes_path
+        else 
+            render 'new'
+        end 
+    end 
+
+    private 
+
+    def yoga_class_params
+        params.require(:yoga_class).permit(
+            :date,
+            :time,
+            :style, 
+            :teacher,
+            :studio
+            )
+    end 
 end
