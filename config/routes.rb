89d14f0efac495190e3abe4cb17a1215fe3be322
carolyn-manning <carolyn.manning3@gmail.com>
2022-01-23 19:30:14 -0500

    create signup, signin, and log routes. build sign in form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  get '/signup', to: 'users#new'
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout/', to: 'sessions#destroy'
+  
   resources :studios
   resources :users
   resources :teachers
