627ca702ea0384aadd4fd9f76d82ad2988f34133
carolyn-manning <carolyn.manning3@gmail.com>
2022-01-23 19:33:17 -0500

    build create and destroy actions in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+    def new 
+    end 
+
+    def create
+        user = User.find_by(email: params[:user][:email])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else 
+            flash[:message] = "error"
+            redirect_to signin_path
+        end 
+    end 
+
+    def destroy
+        session.clear
+        redirect_to '/'
+    end 
 end
