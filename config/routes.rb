6f5633bbaa5a869843b8bd2bad5bfcd63f6be90b
carolyn-manning <carolyn.manning3@gmail.com>
2022-01-24 01:46:49 -0500

    generate static controller, welcome view, and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
+  root 'static#welcome'
+  
   get '/signup', to: 'users#new'
 
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   delete '/signout/', to: 'sessions#destroy'
-  
+
   resources :studios
   resources :users
   resources :teachers
