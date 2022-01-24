a8a67495045288be0f952ac0d2d2c527a3d1221d
carolyn-manning <carolyn.manning3@gmail.com>
2022-01-24 01:53:51 -0500

    create draft teacher show page. add controller action

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,4 +2,8 @@ class TeachersController < ApplicationController
     def index
         @teachers = Teacher.all
     end 
+
+    def show
+        @teacher = Teacher.find_by_id(params[:id])
+    end 
 end
