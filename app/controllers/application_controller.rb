5b986bdc0c763812e45b281d78bf6375e4232516
carolyn-manning <carolyn.manning3@gmail.com>
2022-01-26 16:10:13 -0500

    modify nav in layout to change if user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,20 @@ class ApplicationController < ActionController::Base
             redirect_to '/'
         end 
     end
+
+    # def updated_nav_bar_if_logged_in_1
+    #     if logged_in?
+    #         link_to "My Account", user_path(current_user)
+    #     else
+    #         link_to 'Sign Up', new_user_path
+    #     end  
+    # end 
+
+    # def updated_nav_bar_if_logged_in_2
+    #     if logged_in?
+    #         <%= link_to "Sign Out", signout_path, method:'delete' %>
+    #     else 
+    #         <%= link_to 'Sign In', signin_path %>
+    #     end  
+    # end 
 end
