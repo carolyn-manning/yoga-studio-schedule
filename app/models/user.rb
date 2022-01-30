9c06211f4a94858ca9a20dbce69cd84a71a46c94
carolyn-manning <carolyn.manning3@gmail.com>
2022-01-30 01:19:24 -0500

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_secure_password
     has_many :user_classes
     has_many :yoga_classes, through: :user_classes
+
+    validates :email, uniqueness: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
 end
