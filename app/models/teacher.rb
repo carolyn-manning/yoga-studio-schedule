76df62c02722262c8acc4dbe4dc2f995b53b5d70
carolyn-manning <carolyn.manning3@gmail.com>
2022-01-31 19:47:36 -0500

    update teacher validations

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,4 +2,6 @@ class Teacher < ApplicationRecord
     has_many :yoga_classes
     has_many :yoga_classes
     has_many :styles, through: :yoga_classes
+
+    validates :first_name, :last_name, uniqueness:true, presence:true
 end
